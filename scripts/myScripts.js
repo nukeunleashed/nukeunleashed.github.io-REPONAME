c7a30f5d3380d1b35dfb95219d393832e6dfe113
sermed <sermed93@gmail.com>
2018-06-21 09:03:06 -0700

    1

---
+++ scripts/myScripts.js
@@ -0,0 +1,11 @@
+// Initialize the slider
+  $(document).ready(function(){
+    $('.slider').bxSlider({
+        captions: true,
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+    });
+  });
+// doc ready function
+
